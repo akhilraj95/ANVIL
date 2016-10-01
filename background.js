ba3bd459d1e716198b1047b6a32fd32eb6531718
Akhil Raj Azhikodan <akhil95raj@gmail.com>
2016-10-01 15:04:53 +0530

    Ajax Request to server
    URL and highlighted text sent to sever

--- background.js
+++ background.js
@@ -23,12 +23,48 @@ chrome.commands.onCommand.addListener(function(command) {
         chrome.tabs.executeScript( {
           code: "window.getSelection().toString();"
         }, function(selection) {
-          alert(selection[0]);
+            // d = document;
+            // var f = d.createElement('form');
+            // f.action = 'https://anvilp1940823839trial.hanatrial.ondemand.com/persistence-with-jpa/';
+            // f.method = 'post';
+            // var i = d.createElement('input');
+            // i.type = 'hidden';
+            // i.name = 'highlightdata';
+            // i.value = selection[0];
+            // f.appendChild(i);
+            // var u = d.createElement('input');
+            // u.type = 'hidden';
+            // u.name = 'url';
+            // u.value = 'test';
+            // f.appendChild(u);
+            // d.body.appendChild(f);
+            // f.submit();
+            TrackURL(selection[0]);
+            //alert(selection[0]);
         });
       }
 
   }
 
+
+  function TrackURL(data)
+  {
+    var http = new XMLHttpRequest();
+    var url = "https://anvilp1940823839trial.hanatrial.ondemand.com/persistence-with-jpa/";
+    var params = "highlightdata="+data+"&url=binny";
+    http.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            alert(http.responseText);
+        }
+    }
+    http.send(params);
+
+  }//end function
 
 
 });
